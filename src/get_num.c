18413e524f7a545bc471d50b9305aac187590eb8
paracite <paraciteofficial@gmail.com>
2025-04-14 05:04:30 +0530

    added_clangd

--- src/get_num.c
+++ src/get_num.c
@@ -5,4 +5,12 @@
 #include <errno.h>
 #include "get_num.h"
 
-
+static void gnFail(const char *fname, const char *msg, 
+                   const char *arg, const char *name){
+    fprintf(stderr, "%s error", fname);
+    if (name != NULL) fprintf(stderr, " (in %s)", name);
+    fprintf(stderr, ": %s\n", msg);
+    if (arg != NULL && *arg != '\0') fprintf(stderr,  " offending text: %s\n", arg);
+    
+    exit(EXIT_FAILURE);
+}
